2591e3f74f02cb626b6b5e6fd6ae431760f3072c
SymuNadeem <msymunnadeem@gmail.com>
2024-11-09 21:08:48 +0600

    hero responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
-import ElevateProperty from "@/components/screen/Home/ElevateProperty/ElevateProperty";
-import ExploreMapping from "@/components/screen/Home/ExploreMapping/ExploreMapping";
-import FasterInvestments from "@/components/screen/Home/FasterInvestments/FasterInvestments";
-import FreqAskQuestions from "@/components/screen/Home/FreqAskQuestions/FreqAskQuestions";
+// import ElevateProperty from "@/components/screen/Home/ElevateProperty/ElevateProperty";
+// import ExploreMapping from "@/components/screen/Home/ExploreMapping/ExploreMapping";
+// import FasterInvestments from "@/components/screen/Home/FasterInvestments/FasterInvestments";
+// import FreqAskQuestions from "@/components/screen/Home/FreqAskQuestions/FreqAskQuestions";
 import Hero from "@/components/screen/Home/Hero/Hero";
-import LuxuryProperties from "@/components/screen/Home/LuxuryProperties/LuxuryProperties";
-import OurClients from "@/components/screen/Home/OurClients/OurClients";
-import OurPartner from "@/components/screen/Home/OurPartner/OurPartner";
-import ThreeStepDream from "@/components/screen/Home/ThreeStepDream/ThreeStepDream";
-import TrendProperties from "@/components/screen/Home/TrendProperties/TrendProperties";
+// import LuxuryProperties from "@/components/screen/Home/LuxuryProperties/LuxuryProperties";
+// import OurClients from "@/components/screen/Home/OurClients/OurClients";
+// import OurPartner from "@/components/screen/Home/OurPartner/OurPartner";
+// import ThreeStepDream from "@/components/screen/Home/ThreeStepDream/ThreeStepDream";
+// import TrendProperties from "@/components/screen/Home/TrendProperties/TrendProperties";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <LuxuryProperties />
+      {/* <LuxuryProperties />
       <FasterInvestments />
       <TrendProperties />
       <ElevateProperty />
@@ -21,7 +21,7 @@ export default function Home() {
       <ThreeStepDream />
       <OurPartner />
       <OurClients />
-      <FreqAskQuestions />
+      <FreqAskQuestions /> */}
     </>
   );
 }
--- src/components/screen/Home/Hero/Hero.jsx
+++ src/components/screen/Home/Hero/Hero.jsx
@@ -2,46 +2,44 @@ import { heroItems } from "@/data/heroData";
 import Image from "next/image";
 import searchIcon from "../../../../assets/home/hero-search-icon.svg";
 
-
-
 const Hero = () => {
   return (
-    <section className="bg-[url('/hero-banner.png')] w-full h-[900px] bg-no-repeat bg-cover flex items-center justify-center">
+    <section className="bg-[url('/hero-banner.png')] w-full h-[292px] lg:h-[900px] bg-no-repeat bg-cover flex items-center justify-center">
       <div className="container_fluid">
-        <div className="flex flex-col items-center justify-center space-y-8">
-          <div className="max-w-[903px] mx-auto space-y-3">
-            <h1 className="text-[72px] font-Merriweather font-bold leading-[93px] text-white text-center">
+        <div className="flex flex-col items-center justify-center space-y-4 lg:space-y-8">
+          <div className="max-w-[903px] mx-auto space-y-2 lg:space-y-3">
+            <h1 className="text-2xl lg:text-[72px] lg:leading-[93px] font-Merriweather font-bold  text-white text-center">
               The Future of Real Estate
             </h1>
-            <h3 className="max-w-[749px] mx-auto text-2xl leading-9 text-center text-white">
+            <h3 className="max-w-max sm:max-w-[520px] lg:max-w-[749px] mx-auto text-sm leading-[18px] lg:text-2xl lg:leading-9 text-center text-white">
               Experience a smarter, faster way to find your perfect property.
               Discover luxurious homes and investment opportunities tailored to
               your lifestyle, all at your fingertips.
             </h3>
           </div>
-          <div className="h-[98px] pr-6 bg-white border-[.5px] border-[#F4F4F4] hero-info-box rounded-full flex items-center justify-between gap-12">
+          <div className="h-12 lg:h-[98px] pr-2.5 lg:pr-6 bg-white border-[.5px] border-[#F4F4F4] hero-info-box rounded-full flex items-center justify-between gap-5 lg:gap-12">
             <div className="flex items-center">
               {heroItems.map((item, index) => (
                 <div key={index} className="flex items-center">
-                  <button className="px-6 flex items-center gap-3">
-                    <Image src={item.icon} alt={`${item.title}-hero`} />
+                  <button className="px-2.5 lg:px-6 flex items-center gap-[5px] lg:gap-3">
+                    <Image src={item.icon} alt={`${item.title}-hero`} className="w-4 h-4 lg:w-7 lg:h-7" />
                     <div className="flex flex-col items-start">
-                      <h4 className="text-lg leading-[25px] font-bold text-darkBlue">
+                      <h4 className="text-sm leading-[19px] lg:text-lg lg:leading-[25px]   font-semibold lg:font-bold text-grey600 lg:text-darkBlue">
                         {item.title}
                       </h4>
-                      <h5 className="text-sm text-grey600 leading-[19px]">
+                      <h5 className="hidden lg:block text-sm text-grey600 leading-[19px]">
                         {item.description}
                       </h5>
                     </div>
                   </button>
                   {index < heroItems.length - 1 && (
-                    <div className="w-[1px] h-8 mx-5 bg-grayLine"></div>
+                    <div className="w-[1px] h-5 lg:h-8 mx-1.5 lg:mx-5 bg-grayLine"></div>
                   )}
                 </div>
               ))}
             </div>
-            <button className="w-16 h-16 rounded-full bg-primary hero-search-box flex items-center justify-center">
-              <Image src={searchIcon} alt="searchIcon" />
+            <button className="w-8 h-8 lg:w-16 lg:h-16 rounded-full bg-primary hero-search-box flex items-center justify-center">
+              <Image src={searchIcon} alt="searchIcon" className="w-3 h-3 lg:w-7 lg:h-7" />
             </button>
           </div>
         </div>
