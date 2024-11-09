6f8fa1b6aba6f85b0de7deccea4c0b333d8dd422
SymuNadeem <msymunnadeem@gmail.com>
2024-11-09 12:00:53 +0600

    trend properties sections start

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import FasterInvestments from "@/components/screen/Home/FasterInvestments/FasterInvestments";
 import Hero from "@/components/screen/Home/Hero/Hero";
 import LuxuryProperties from "@/components/screen/Home/LuxuryProperties/LuxuryProperties";
+import TrendProperties from "@/components/screen/Home/TrendProperties/TrendProperties";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <LuxuryProperties />
       <FasterInvestments />
+      <TrendProperties />
     </>
   );
 }
---
+++ src/components/screen/Home/TrendProperties/TrendProperties.jsx
@@ -0,0 +1,25 @@
+const TrendProperties = () => {
+  return (
+    <section>
+      <div className="container_fluid">
+        <div className="pt-20 pb-18">
+          <div className="flex flex-col items-center justify-center space-y-[6px] md:space-y-2">
+            <h2 className="text-xl md:text-[48px] md:leading-[67px] font-bold text-darkBlue text-center">
+              Trending <span className="text-primary">Properties</span>
+            </h2>
+            <h4 className="max-w-[784px] mx-auto text-sm md:text-lg text-grey700 text-center">
+              Discover the most sought-after properties in real-time, powered by
+              AI to bring you top picks based on current market demand and
+              trends.
+            </h4>
+          </div>
+          <div className="mt-12 grid grid-cols-3 gap-6">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TrendProperties;
