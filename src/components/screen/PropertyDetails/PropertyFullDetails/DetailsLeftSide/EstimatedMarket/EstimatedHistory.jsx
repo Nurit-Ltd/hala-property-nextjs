287fe9324bd045573db7ba677452e85cd1a5e656
SymuNadeem <msymunnadeem@gmail.com>
2024-11-11 22:57:00 +0600

    line chart used

--- src/components/screen/PropertyDetails/PropertyFullDetails/DetailsLeftSide/EstimatedMarket/EstimatedHistory.jsx
+++ src/components/screen/PropertyDetails/PropertyFullDetails/DetailsLeftSide/EstimatedMarket/EstimatedHistory.jsx
@@ -1,10 +1,15 @@
+import EstimatedChart from "./EstimatedChart";
 
 const EstimatedHistory = () => {
   return (
     <div className="mt-4 p-6 bg-white border border-grey400 rounded-lg">
       <div className="flex items-center justify-between">
         <h4 className="text-lg font-bold text-grey700">Estimated history</h4>
+        
       </div>
+      <div>
+          <EstimatedChart />
+        </div>
     </div>
   );
 };
