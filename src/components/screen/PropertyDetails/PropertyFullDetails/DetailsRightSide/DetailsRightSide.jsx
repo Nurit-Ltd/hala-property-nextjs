e02bccc42d9d49c0872c28abeb64473ca413c4eb
SymuNadeem <msymunnadeem@gmail.com>
2024-11-12 12:57:08 +0600

    userinfo and contact right side

--- src/components/screen/PropertyDetails/PropertyFullDetails/DetailsRightSide/DetailsRightSide.jsx
+++ src/components/screen/PropertyDetails/PropertyFullDetails/DetailsRightSide/DetailsRightSide.jsx
@@ -1,8 +1,9 @@
+import UserInfoAndContact from "./UserInfoAndContact";
 
 const DetailsRightSide = () => {
   return (
-    <div>
-      DetailsRightSide
+    <div className="px-6 pt-[30px] pb-6 border border-grey400 rounded-lg bg-white right-side-box-property">
+      <UserInfoAndContact />
     </div>
   );
 };
