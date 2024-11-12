f42ff587ef111f6d5fd552c194c4c6f6787bec27
Al Amin <alaminpgmr@gmail.com>
2024-11-12 15:36:31 +0600

    "Added @radix-ui/react-tabs and @radix-ui/react-presence dependencies, updated package-lock.json, added new components and functionality to BuyPage, and updated package.json"

--- src/app/buy/page.jsx
+++ src/app/buy/page.jsx
@@ -1,12 +1,73 @@
 import BuyHero from "@/components/screen/Buy/BuyHero";
 import SearchFilter from "@/components/screen/Buy/SearchFilter";
+import ProductCard from "@/components/shared/ProductCard/ProductCard";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { cardData } from "@/data/productCardData";
 
 const BuyPage = () => {
   return (
     <>
       <BuyHero />
-      <section className="container_fluid">
+      <section className="container_fluid pb-15 lg:pb-24">
         <SearchFilter />
+
+        <div className="space-y-6">
+          <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
+            {cardData.map((card) => (
+              <ProductCard
+                key={card.id}
+                card={card}
+              />
+            ))}
+            {cardData.map((card) => (
+              <ProductCard
+                key={card.id}
+                card={card}
+              />
+            ))}
+          </div>
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious href="#" />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink
+                  isActive={true}
+                  href="#"
+                >
+                  1
+                </PaginationLink>
+              </PaginationItem>{" "}
+              <PaginationItem>
+                <PaginationLink href="#">2</PaginationLink>
+              </PaginationItem>{" "}
+              <PaginationItem>
+                <PaginationLink href="#">3</PaginationLink>
+              </PaginationItem>{" "}
+              <PaginationItem>
+                <PaginationLink href="#">4</PaginationLink>
+              </PaginationItem>{" "}
+              <PaginationItem>
+                <PaginationLink href="#">5</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext href="#" />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </div>
       </section>
     </>
   );
