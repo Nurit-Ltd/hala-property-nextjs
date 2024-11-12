aa55a4075c13dc3aeaff3e655f59cd468a01c163
Al Amin <alaminpgmr@gmail.com>
2024-11-12 16:17:36 +0600

    "Wrapped SearchFilter and TabFilter in a div with 'hidden lg:block' class, and Pagination in a div with 'hidden sm:block' class, and added pt-8 lg:pt-0 to h2 element."

--- src/app/buy/page.jsx
+++ src/app/buy/page.jsx
@@ -18,11 +18,13 @@ const BuyPage = () => {
     <>
       <BuyHero />
       <section className="container_fluid pb-15 lg:pb-24">
-        <SearchFilter />
-        <TabFilter />
+        <div className="hidden lg:block">
+          <SearchFilter />
+          <TabFilter />
+        </div>
 
         <div className="space-y-6">
-          <h2 className="section-header-title !text-2xl">
+          <h2 className="section-header-title !text-2xl pt-8 lg:pt-0">
             {cardData.length * 3} Properties found
           </h2>
           <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
@@ -45,39 +47,41 @@ const BuyPage = () => {
               />
             ))}
           </div>
-          <Pagination>
-            <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious href="#" />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink
-                  isActive={true}
-                  href="#"
-                >
-                  1
-                </PaginationLink>
-              </PaginationItem>{" "}
-              <PaginationItem>
-                <PaginationLink href="#">2</PaginationLink>
-              </PaginationItem>{" "}
-              <PaginationItem>
-                <PaginationLink href="#">3</PaginationLink>
-              </PaginationItem>{" "}
-              <PaginationItem>
-                <PaginationLink href="#">4</PaginationLink>
-              </PaginationItem>{" "}
-              <PaginationItem>
-                <PaginationLink href="#">5</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationNext href="#" />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
+          <div className="hidden sm:block">
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <PaginationPrevious href="#" />
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink
+                    isActive={true}
+                    href="#"
+                  >
+                    1
+                  </PaginationLink>
+                </PaginationItem>{" "}
+                <PaginationItem>
+                  <PaginationLink href="#">2</PaginationLink>
+                </PaginationItem>{" "}
+                <PaginationItem>
+                  <PaginationLink href="#">3</PaginationLink>
+                </PaginationItem>{" "}
+                <PaginationItem>
+                  <PaginationLink href="#">4</PaginationLink>
+                </PaginationItem>{" "}
+                <PaginationItem>
+                  <PaginationLink href="#">5</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationNext href="#" />
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          </div>
         </div>
       </section>
     </>
