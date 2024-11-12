d704d1834814acb93b96af7332d90d1e4d242def
Al Amin <alaminpgmr@gmail.com>
2024-11-12 16:14:31 +0600

    "Added icons to TabFilter component and updated button styles"

--- src/components/screen/Buy/TabFilter.jsx
+++ src/components/screen/Buy/TabFilter.jsx
@@ -1,4 +1,11 @@
 "use client";
+import {
+  DiamondIcon,
+  NewIcon,
+  RatingIcon,
+  RentalIcon,
+  RoIcon,
+} from "@/components/shared/SVG";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { SlidersHorizontal } from "lucide-react";
@@ -9,11 +16,11 @@ const TabFilter = () => {
 
   // Define an array of tab objects
   const tabs = [
-    { id: 1, label: "In Demand" },
-    { id: 2, label: "ROI" },
-    { id: 3, label: "Project Rating" },
-    { id: 4, label: "New" },
-    { id: 5, label: "Rental Yield" },
+    { id: 1, label: "In Demand", icon: <DiamondIcon /> },
+    { id: 2, label: "ROI", icon: <RoIcon /> },
+    { id: 3, label: "Project Rating", icon: <RatingIcon /> },
+    { id: 4, label: "New", icon: <NewIcon /> },
+    { id: 5, label: "Rental Yield", icon: <RentalIcon /> },
   ];
 
   return (
@@ -33,12 +40,12 @@ const TabFilter = () => {
             onClick={() => setActiveTab(tab.id)}
             variant="outline"
             className={cn(
-              "rounded-full bg-[#F6F5F1] border-grey400 py-2.5 px-4 gap-2",
+              "rounded-full bg-[#F6F5F1] text-[#323B49] border-grey400 py-2.5 px-4 gap-2",
               activeTab === tab.id &&
                 "bg-primary/5 text-primary border border-primary"
             )}
           >
-            <SlidersHorizontal /> {tab.label}
+            {tab.icon} {tab.label}
           </Button>
         ))}
       </div>
