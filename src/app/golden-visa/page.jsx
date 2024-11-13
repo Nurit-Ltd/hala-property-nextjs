89d389fc13c50cc48a53245ca65e834759fd2c6c
SymuNadeem <msymunnadeem@gmail.com>
2024-11-13 16:09:55 +0600

    Service Fees & Refund Policy

--- src/app/golden-visa/page.jsx
+++ src/app/golden-visa/page.jsx
@@ -3,6 +3,7 @@ import FreSecAndOpportunity from "@/components/screen/GoldenVisa/FreSecAndOpport
 import HeroVisa from "@/components/screen/GoldenVisa/HeroVisa/HeroVisa";
 import InvestInProperty from "@/components/screen/GoldenVisa/InvestInProperty/InvestInProperty";
 import OurProcess from "@/components/screen/GoldenVisa/OurProcess/OurProcess";
+import ServiceRefund from "@/components/screen/GoldenVisa/ServiceRefund/ServiceRefund";
 
 const GoldenVisaPage = () => {
   return (
@@ -11,6 +12,7 @@ const GoldenVisaPage = () => {
       <FreSecAndOpportunity />
       <InvestInProperty />
       <OurProcess />
+      <ServiceRefund />
     </>
   );
 };
