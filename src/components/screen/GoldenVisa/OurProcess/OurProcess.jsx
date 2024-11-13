46bc735aa0a944e9d8f78cdf87bc32230983dc4c
SymuNadeem <msymunnadeem@gmail.com>
2024-11-13 22:20:27 +0600

    step folder and design

--- src/components/screen/GoldenVisa/OurProcess/OurProcess.jsx
+++ src/components/screen/GoldenVisa/OurProcess/OurProcess.jsx
@@ -4,15 +4,15 @@ const OurProcess = () => {
   return (
     <section>
       <div className="container_fluid">
-        <div className="py-25">
+        <div className="py-10 md:py-25">
           <div className="max-w-[1038px] mx-auto">
-            <div className="space-y-5 max-w-[690px] mx-auto">
+            <div className="space-y-2 md:space-y-5 max-w-[690px] mx-auto">
               <h4 className="section-header-sub-title text-center">
                 Our Process
               </h4>
-              <div className="space-y-4">
+              <div className="space-y-2 md:space-y-4">
                 <h2 className="section-header-title text-darkBlue text-center">Fast, Efficient, and All Online</h2>
-                <h4 className="text-lg text-darkBlue text-center">We streamline the entire process for you. Here’s how it works:</h4>
+                <h4 className="text-base md:text-lg text-darkBlue text-center">We streamline the entire process for you. Here’s how it works:</h4>
               </div>
             </div>
             <ProcessCard />
--- src/components/screen/GoldenVisa/OurProcess/ProcessCard.jsx
+++ src/components/screen/GoldenVisa/OurProcess/ProcessCard.jsx
@@ -6,23 +6,23 @@ const ProcessCard = () => {
     <div className="mt-6 md:mt-15">
       <div className="grid grid-cols-1  md:grid-cols-3 gap-6 justify-items-center">
         {/* First three cards */}
-        {stepCardData.slice(0, 3).map((benefit, index) => (
+        {stepCardData.slice(0, 3).map((step, index) => (
           <div
             key={index}
-            className="w-full p-6 rounded-lg lg:rounded-2xl border border-[#e5ecf8] bg-[#eef2f9] text-center relative"
+            className="w-full min-h-[126px] md:min-h-[153px] p-4 md:p-6 rounded-lg lg:rounded-2xl border border-[#e5ecf8] bg-[#eaedf2] text-center relative flex items-center justify-center"
           >
             <h4 className="text-base lg:text-lg text-grey700">
-              {benefit.description}
+              {step.description}
             </h4>
-            <div className="absolute top-[-1px] left-[40%]">
-              <div className="w-[100px] h-[30px] bg-[#f7f5f2] border border-t-0 border-[#e5ecf8] flex items-center justify-center rounded-br-2xl rounded-bl-2xl relative">
-                <div className="w-[68px] bg-primary px-3 py-0.5 rounded-lg text-white text-sm font-semibold">
-                  {benefit.title}
+            <div className="absolute top-[-1px] left-1/2 transform -translate-x-1/2">
+              <div className="w-[85px] h-[32px] bg-[#f7f5f2] border border-t-0 border-[#e5ecf8] flex items-center justify-center rounded-br-2xl rounded-bl-2xl relative">
+                <div className="w-[68px] bg-primary px-3 py-0.5 rounded-lg text-white text-sm font-semibold relative z-20">
+                  {step.title}
                 </div>
-                <div className="absolute top-0 left-[-16px] w-[16px] h-[16px] bg-[#eef2f9] rounded-tr-[16px] border-r-[1px] border-t-[1px] border-[#e5ecf8] tong">
+                <div className="absolute top-[0.5px] left-[-20px] w-[20px] h-[20px] bg-[#eaedf2] rounded-tr-[16px] border-r-[1px] border-t-[1px] border-[#e5ecf8] z-10 tong">
 
                 </div>
-                <div className="absolute top-0 right-[-16px] w-[16px] h-[16px] bg-[#eef2f9] rounded-tl-[16px] border-l-[1px] border-t-[1px] border-[#e5ecf8] tong-two">
+                <div className="absolute top-0 right-[-20px] w-[20px] h-[20px] bg-[#eaedf2] rounded-tl-[16px] border-l-[1px] border-t-[1px] border-[#e5ecf8] z-10 tong-two">
 
                 </div>
               </div>
@@ -32,22 +32,33 @@ const ProcessCard = () => {
 
         {/* Last two cards */}
         <div className="w-full md:col-span-3 md:flex justify-center md:space-x-5 space-y-4 md:space-y-0">
-          {stepCardData.slice(3).map((benefit, index) => (
+          {stepCardData.slice(3).map((step, index) => (
             <div
               key={index}
-              className="w-full md:max-w-[33%] p-6 rounded-lg lg:rounded-2xl border border-primary/[4%] bg-primary/[5%] text-center"
+              className="w-full min-h-[126px] md:min-h-[153px] md:max-w-[33%] p-4 md:p-6 rounded-lg lg:rounded-2xl border border-primary/[4%] bg-[#eaedf2] text-center relative flex items-center justify-center"
             >
-              <div className="w-[68px] bg-primary px-3 py-0.5 rounded-lg text-white text-sm font-semibold">
-                {benefit.title}
-              </div>
-              <h4 className="text-base lg:text-lg text-grey600">
-                {benefit.description}
+              
+              <h4 className="text-base lg:text-lg text-grey700">
+                {step.description}
               </h4>
+              <div className="absolute top-[-1px] left-1/2 transform -translate-x-1/2">
+              <div className="w-[85px] h-[32px] bg-[#f7f5f2] border border-t-0 border-[#e5ecf8] flex items-center justify-center rounded-br-2xl rounded-bl-2xl relative">
+                <div className="w-[68px] bg-primary px-3 py-0.5 rounded-lg text-white text-sm font-semibold relative z-20">
+                  {step.title}
+                </div>
+                <div className="absolute top-[0.5px] left-[-20px] w-[20px] h-[20px] bg-[#eaedf2] rounded-tr-[16px] border-r-[1px] border-t-[1px] border-[#e5ecf8] z-10 tong">
+
+                </div>
+                <div className="absolute top-0 right-[-20px] w-[20px] h-[20px] bg-[#eaedf2] rounded-tl-[16px] border-l-[1px] border-t-[1px] border-[#e5ecf8] z-10 tong-two">
+
+                </div>
+              </div>
+            </div>
             </div>
           ))}
         </div>
       </div>
-      <div className="mt-12 flex items-center justify-center">
+      <div className="mt-8 md:mt-12 flex items-center justify-center">
         <Button48 buttonTitle="Book a Free Consultation" />
       </div>
     </div>
