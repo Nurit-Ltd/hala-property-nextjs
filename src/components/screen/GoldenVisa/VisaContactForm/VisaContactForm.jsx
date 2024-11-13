f3adf2bac37ee4f24d68daa52617e7ed96a03a4c
SymuNadeem <msymunnadeem@gmail.com>
2024-11-13 18:15:34 +0600

    form done

--- src/components/screen/GoldenVisa/VisaContactForm/VisaContactForm.jsx
+++ src/components/screen/GoldenVisa/VisaContactForm/VisaContactForm.jsx
@@ -6,7 +6,7 @@ const VisaContactForm = () => {
   return (
     <section>
       <div className="container_fluid">
-        <div className="pt-30 pb-20 grid md:grid-cols-2 gap-20">
+        <div className="pt-8 md:pt-30 pb-12 md:pb-20 grid md:grid-cols-2 gap-6 lg:gap-20">
           <div className="max-w-[280px] mx-auto md:mx-0 md:max-w-[469px]">
             <h2 className="section-header-title text-darkBlue text-center xl:text-start">
               We’d Love to{" "}
@@ -23,7 +23,7 @@ const VisaContactForm = () => {
               From{" "}
               <span className="relative">
                 {" "}
-                You
+                 You
                 <div className="xl:hidden absolute right-[-20px] md:right-[-18px] xl:right-[-45px] top-[-5px] md:top-[-2px] xl:top-[-15px]">
                   <Image
                     src={shapesText}
@@ -32,7 +32,7 @@ const VisaContactForm = () => {
                   />
                 </div>
               </span>{" "}
-              – <span className="text-primary"> Get in Touch!</span>
+              -<span className="text-primary"> Get in Touch!</span>
             </h2>
           </div>
           <div>
