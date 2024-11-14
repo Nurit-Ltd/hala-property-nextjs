255af7f6a452c02397af389f4176861f3c920780
Al Amin <alaminpgmr@gmail.com>
2024-11-14 14:01:58 +0600

    updated

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -1,13 +1,18 @@
 "use client";
 // CONTACT US PAGE
-import React from "react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 import Image from "next/image";
 
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaXTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
 import { RiHeadphoneFill, RiPhoneFill, RiWhatsappFill } from "react-icons/ri";
-import { FaFacebookF, FaXTwitter, FaLinkedinIn, FaInstagram, FaYoutube } from "react-icons/fa6";
 
 import contactPage from "@/assets/blog/contact-page.jpg";
 
@@ -41,67 +46,131 @@ const ContactUs = () => {
         {/* Heading */}
         <div className="  pt-10 xl:pt-[100px] flex flex-col items-center max-w-[1015px] mx-auto px-4 xl:px-0  gap-2  ">
           <h2 className="text-xl lg:text-3xl  xl:text-[48px] font-bold  text-gray-900 leading-tight">
-            Let's <span className="text-primary">Talk</span>
+            Let&apos;s <span className="text-primary">Talk</span>
           </h2>
-          <p className="text-base xl:text-lg   text-center  text-gray-700">Whether you need help finding your perfect home or looking for the ideal investment property, contact us today. Our expert team of property consultants is dedicated to providing you with exceptional service and helping you find your dream home.</p>
+          <p className="text-base xl:text-lg   text-center  text-gray-700">
+            Whether you need help finding your perfect home or looking for the
+            ideal investment property, contact us today. Our expert team of
+            property consultants is dedicated to providing you with exceptional
+            service and helping you find your dream home.
+          </p>
         </div>
 
         {/* Contact Form */}
         <div className="absolute z-20 left-0 top-[240px] xl:top-1/2 w-full flex justify-center">
           <div className=" mx-[16px]   p-[20px] xl:p-[30px] w-full max-w-[1280px] xl:mx-auto bg-white border   formBoxShadow  border-[#CBD5E0] rounded-2xl  ">
-            <form onSubmit={handleSubmit(onSubmit)} className="  ">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="  "
+            >
               <div className="   flex flex-col lg:flex-row justify-between gap-4 xl:gap-6">
                 {/* Name */}
                 <div className="w-full">
-                  <label className="block text-[#0D152E] text-base font-semibold  mb-2">Your Name</label>
-                  <input type="text" {...register("name", { required: "Name is required" })} className="appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter your first & last name" />
-                  {errors.name && <p className="text-red-500 text-xs italic">{errors.name.message}</p>}
+                  <label className="block text-[#0D152E] text-base font-semibold  mb-2">
+                    Your Name
+                  </label>
+                  <input
+                    type="text"
+                    {...register("name", { required: "Name is required" })}
+                    className="appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="Enter your first & last name"
+                  />
+                  {errors.name && (
+                    <p className="text-red-500 text-xs italic">
+                      {errors.name.message}
+                    </p>
+                  )}
                 </div>
 
                 {/* Email */}
                 <div className="w-full">
-                  <label className="block text-[#0D152E] text-base font-semibold  mb-2">Your Email</label>
+                  <label className="block text-[#0D152E] text-base font-semibold  mb-2">
+                    Your Email
+                  </label>
                   <input
                     type="email"
                     {...register("email", {
                       required: "Email is required",
                       pattern: {
-                        value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                        value:
+                          /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
                         message: "Please enter a valid email address",
                       },
                     })}
                     className="appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     placeholder="Enter your email address"
                   />
-                  {errors.email && <p className="text-red-500 text-xs italic">{errors.email.message}</p>}
+                  {errors.email && (
+                    <p className="text-red-500 text-xs italic">
+                      {errors.email.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
               <div className="mt-4 xl:mt-6 flex flex-col lg:flex-row justify-between gap-4 xl:gap-6">
                 {/* Phone */}
                 <div className="w-full">
-                  <label className="block text-[#0D152E] text-base font-semibold  mb-2">Phone</label>
-                  <input type="tel" {...register("phone", { required: "Phone is required" })} className="appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter your pohone number" />
-                  {errors.phone && <p className="text-red-500 text-xs italic">{errors.phone.message}</p>}
+                  <label className="block text-[#0D152E] text-base font-semibold  mb-2">
+                    Phone
+                  </label>
+                  <input
+                    type="tel"
+                    {...register("phone", { required: "Phone is required" })}
+                    className="appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="Enter your pohone number"
+                  />
+                  {errors.phone && (
+                    <p className="text-red-500 text-xs italic">
+                      {errors.phone.message}
+                    </p>
+                  )}
                 </div>
 
                 {/* Subject */}
                 <div className="w-full">
-                  <label className="block text-[#0D152E] text-base font-semibold  mb-2">Subject</label>
-                  <input type="text" {...register("subject", { required: "Subject is required" })} className="appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter a subject line" />
-                  {errors.subject && <p className="text-red-500 text-xs italic">{errors.subject.message}</p>}
+                  <label className="block text-[#0D152E] text-base font-semibold  mb-2">
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    {...register("subject", {
+                      required: "Subject is required",
+                    })}
+                    className="appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="Enter a subject line"
+                  />
+                  {errors.subject && (
+                    <p className="text-red-500 text-xs italic">
+                      {errors.subject.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
               {/* Message */}
               <div className="mt-4 xl:mt-6">
-                <label className="block text-[#0D152E] text-base font-semibold  mb-2">Message</label>
-                <textarea {...register("message", { required: "Message is required" })} className=" h-[148px] lg:h-[220px] appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Write your message" rows={4}></textarea>
-                {errors.message && <p className="text-red-500 text-xs italic">{errors.message.message}</p>}
+                <label className="block text-[#0D152E] text-base font-semibold  mb-2">
+                  Message
+                </label>
+                <textarea
+                  {...register("message", { required: "Message is required" })}
+                  className=" h-[148px] lg:h-[220px] appearance-none border rounded-lg w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Write your message"
+                  rows={4}
+                ></textarea>
+                {errors.message && (
+                  <p className="text-red-500 text-xs italic">
+                    {errors.message.message}
+                  </p>
+                )}
               </div>
 
               {/* Send Button */}
-              <button type="submit" className=" mt-[36px] h-14 w-full py-3 px-4 bg-primary submitBtnShadow     hover:bg-blue-700 text-white font-bold rounded-lg focus:outline-none tracking-[2%] focus:shadow-outline">
+              <button
+                type="submit"
+                className=" mt-[36px] h-14 w-full py-3 px-4 bg-primary submitBtnShadow     hover:bg-blue-700 text-white font-bold rounded-lg focus:outline-none tracking-[2%] focus:shadow-outline"
+              >
                 Send
               </button>
             </form>
@@ -116,14 +185,22 @@ const ContactUs = () => {
               <div className="w-[80px] h-[80px] rounded-lg bg-[#FFFFFF1A] flex justify-center items-center">
                 <RiHeadphoneFill className="text-[40px] text-white" />
               </div>
-              <h3 className="text-2xl mt-8 font-bold text-white">Help & support</h3>
+              <h3 className="text-2xl mt-8 font-bold text-white">
+                Help & support
+              </h3>
               <p className=" text-base xl:text-lg mt-4">
                 Email:{" "}
-                <a className="text-[#3371E9] text-lg" href="mailto:info@halaproperty.com">
+                <a
+                  className="text-[#3371E9] text-lg"
+                  href="mailto:info@halaproperty.com"
+                >
                   info@halaproperty.com
                 </a>
               </p>
-              <p className=" text-sm xl:text-base mt-2 text-[#CBD5E0]">For help with a current product or service or refer to FAQs and developer tools.</p>
+              <p className=" text-sm xl:text-base mt-2 text-[#CBD5E0]">
+                For help with a current product or service or refer to FAQs and
+                developer tools.
+              </p>
             </div>
             {/* Call */}
             <div className="w-full max-w-[405px] h-[308px] rounded-2xl p-6 text-white border border-[#FFFFFF33]">
@@ -132,16 +209,24 @@ const ContactUs = () => {
               </div>
               <h3 className="text-2xl mt-8 font-bold text-white">Call us</h3>
               <p className="text-base xl:text-lg mt-4">
-                <a className="text-[#3371E9] text-base xl:text-lg" href="tel: + 971 362 2254">
+                <a
+                  className="text-[#3371E9] text-base xl:text-lg"
+                  href="tel: + 971 362 2254"
+                >
                   + 971 362 2254
                 </a>
               </p>
               <p className="text-base xl:text-lg ">
-                <a className="text-[#3371E9] text-base xl:text-lg" href="tel: + 971 658 3879">
+                <a
+                  className="text-[#3371E9] text-base xl:text-lg"
+                  href="tel: + 971 658 3879"
+                >
                   + 971 658 3879
                 </a>
               </p>
-              <p className="text-sm xl:text-base mt-2 text-[#CBD5E0]">Call us to speak to a member of our team.</p>
+              <p className="text-sm xl:text-base mt-2 text-[#CBD5E0]">
+                Call us to speak to a member of our team.
+              </p>
             </div>
             {/* Whatsapp */}
             <div className="w-full max-w-[405px] h-[308px] rounded-2xl p-6 text-white border border-[#FFFFFF33]">
@@ -150,7 +235,10 @@ const ContactUs = () => {
               </div>
               <h3 className="text-2xl mt-8 font-bold text-white">WhatsApp</h3>
               <p className="text-base xl:text-lg mt-4">
-                <a className="text-[#3371E9] text-base xl:text-lg" href="tel: + 971 362 2254">
+                <a
+                  className="text-[#3371E9] text-base xl:text-lg"
+                  href="tel: + 971 362 2254"
+                >
                   + 971 362 2254
                 </a>
               </p>
@@ -167,19 +255,35 @@ const ContactUs = () => {
         <div className="w-full max-w-[1280px] mx-auto ">
           <div className="flex flex-col xl:flex-row  xl:justify-between items-center gap-[40px] xl:items-stretch  ">
             <div className="w-full max-w-[600px]">
-              <Image className=" rounded-2xl   h-full" src={contactPage} alt="contact us" />
+              <Image
+                className=" rounded-2xl   h-full"
+                src={contactPage}
+                alt="contact us"
+              />
             </div>
             <div className="w-full max-w-[640px]  flex flex-col justify-center   ">
               <p className=" mt-[6px] xl:mt-3 text-sm xl:text-lg      text-gray-700">
-                If you have any questions or feedback, please don't hesitate to contact us. Our team is dedicated to providing you with the best service possible. You can reach us through email at{" "}
-                <a className="text-[#0954E5]" href="mailto:info@halaproperty.com">
+                If you have any questions or feedback, please don&apos;t
+                hesitate to contact us. Our team is dedicated to providing you
+                with the best service possible. You can reach us through email
+                at{" "}
+                <a
+                  className="text-[#0954E5]"
+                  href="mailto:info@halaproperty.com"
+                >
                   info@halaproperty.com
                 </a>
-                , or by filling out the contact form on our website. We look forward to hearing from you!
+                , or by filling out the contact form on our website. We look
+                forward to hearing from you!
               </p>
               <div className="flex gap-4 mt-6 xl:mt-[60px]">
                 {socialLinks.map((link, index) => (
-                  <a alt={link.title} key={index} href={link.link} className="w-[32px] h-[32px] rounded-full bg-darkBlue flex items-center justify-center text-white">
+                  <a
+                    alt={link.title}
+                    key={index}
+                    href={link.link}
+                    className="w-[32px] h-[32px] rounded-full bg-darkBlue flex items-center justify-center text-white"
+                  >
                     <link.icon className="text-base" />
                   </a>
                 ))}
