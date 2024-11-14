7239b76aff087503a6692e49f7cd8cf2ce69db76
SymuNadeem <msymunnadeem@gmail.com>
2024-11-14 10:14:55 +0600

    our vision

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,9 +1,11 @@
 import AboutHero from "@/components/screen/AboutUs/AboutHero/AboutHero";
+import OurVision from "@/components/screen/AboutUs/OurVision/OurVision";
 
 const AboutUsPage = () => {
   return (
     <>
       <AboutHero />
+      <OurVision />
     </>
   );
 };
