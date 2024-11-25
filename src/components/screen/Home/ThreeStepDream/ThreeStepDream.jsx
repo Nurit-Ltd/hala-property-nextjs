ddd93dafd801c34a45ba4e8f1ba0d302ad7e5bde
Al Amin <alaminpgmr@gmail.com>
2024-11-25 20:34:44 +0600

    updated home page complete

--- src/components/screen/Home/ThreeStepDream/ThreeStepDream.jsx
+++ src/components/screen/Home/ThreeStepDream/ThreeStepDream.jsx
@@ -6,23 +6,28 @@ import "swiper/css/pagination";
 import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import ThreeStepDreamCard from "./ThreeStepDreamCard";
+
 const ThreeStepDream = () => {
   return (
     <section className="bg-secBg">
       <div className="container_fluid">
         <div className="pt-6 pb-8 md:pt-15 md:pb-25">
           <h2 className="section-header-title text-darkBlue text-center">
-            3 Steps to Find Your <span className="text-primary">Dream Home</span>
+            3 Steps to Find Your{" "}
+            <span className="text-primary">Dream Home</span>
           </h2>
           <div className="hidden lg:block">
             <div className="mt-12 grid grid-cols-2 xl:grid-cols-3 gap-5">
               {stepsCard.map((step) => (
-                <ThreeStepDreamCard key={step.id} step={step} />
+                <ThreeStepDreamCard
+                  key={step.id}
+                  step={step}
+                />
               ))}
             </div>
           </div>
-          {/* slider use for small screen */}
-          <div className="mt-6 pt-6 lg:hidden">
+          {/* Slider for small screens */}
+          <div className="mt-6 lg:hidden">
             <Swiper
               spaceBetween={16}
               slidesPerView={1.2}
@@ -45,7 +50,7 @@ const ThreeStepDream = () => {
                 clickable: true,
                 el: ".custom-pagination",
                 renderBullet: (index, className) => {
-                  return `<span className="${className} custom-bullet"></span>`;
+                  return `<span class="${className} custom-bullet"></span>`;
                 },
               }}
               navigation
@@ -53,8 +58,14 @@ const ThreeStepDream = () => {
               className="mySwiper"
             >
               {stepsCard.map((step) => (
-                <SwiperSlide key={step.id} className="!pb-6 lg:pb-0">
-                  <ThreeStepDreamCard key={step.id} step={step} />
+                <SwiperSlide
+                  key={step.id}
+                  className="!pb-6 lg:pb-0"
+                >
+                  <ThreeStepDreamCard
+                    key={step.id}
+                    step={step}
+                  />
                 </SwiperSlide>
               ))}
             </Swiper>
